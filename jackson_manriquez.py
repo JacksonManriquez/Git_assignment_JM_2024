061daf9bd825a145365e5e29b129540b99ccd006
pukuku <pukuku101@gmail.com>
2024-04-03 13:14:45 -0700

    Added jackson_manriquez.py with rect_surface_area func

--- jackson_manriquez.py
+++ jackson_manriquez.py
@@ -5,10 +5,8 @@
 # 2. rect_solid_area (length, width, height) which will return the area of a solid rectangular object
 
 # The following four lines are just there to make the code work without errors until functions are added
-def rect_area(x, y, z):
+def rect_area(x, y):
     return 1
-length = 1; width = 1; height = 1
-rect_area(length, width, height)
 
 
 ###Jackson Manriquez's function
@@ -26,7 +24,7 @@ length = int(input("Enter the length of the the object as in integer: "))
 width = int(input("Enter the width of the the object as in integer: "))
 height = int(input("Enter the height of the the object as in integer: "))
 
-surface_area = rect_solid_area(length, width, height)
+surface_area = rect_surface_area(length, width, height)
 
 print("Length = ", length, "Width = ", width, "Height = ", height)
 print("Total Surface Area = ", surface_area)
