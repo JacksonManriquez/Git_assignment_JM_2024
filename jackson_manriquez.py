1e6f48eaafe8ce1728825e55963599907325f527
JacksonManriquez <165833971+JacksonManriquez@users.noreply.github.com>
2024-04-07 22:13:31 -0700

    Update jackson_manriquez.py
    Changed rect_area for functionality/scalability. also changed comments for more concise reading experience

--- jackson_manriquez.py
+++ jackson_manriquez.py
@@ -1,12 +1,6 @@
-# CS 16X Git Assignment
-
-# Project requires TWO functions:
-# 1. rect_area (length, width) which will return the area of a rectangle
-# 2. rect_solid_area (length, width, height) which will return the area of a solid rectangular object
-
-# The following four lines are just there to make the code work without errors until functions are added
+# Filled in rect area from for functionality
 def rect_area(x, y):
-    return 1
+    return x * y
 
 
 ###Jackson Manriquez's function
@@ -19,12 +13,12 @@ def rect_surface_area(x, y, z):
     return surface_area
 
 
-# Request the dimension of a solid rectangular object
+# The request for dimenisions
 length = int(input("Enter the length of the the object as in integer: "))
 width = int(input("Enter the width of the the object as in integer: "))
 height = int(input("Enter the height of the the object as in integer: "))
 
 surface_area = rect_surface_area(length, width, height)
-
+# Finishing of functions
 print("Length = ", length, "Width = ", width, "Height = ", height)
 print("Total Surface Area = ", surface_area)
